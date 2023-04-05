cfce6ec8c217af04cea7d85cc0f0bec542d0618c
ChrisUser <cristiano-raimondi@tiscali.it>
2023-04-05 09:58:40 +0200

    Bump to v1.1.10

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -4,11 +4,7 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 const root = createRoot(document.getElementById('root')!) // createRoot(container!) if you use TypeScript
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<App />)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
